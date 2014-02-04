e40781de1f88f4101001db6375a1b6b6cdbbd040
Lars Falk-Petersen <dev@falk-petersen.no>
2014-02-04 14:27:19 +0100

    Small fixes

--- helpers/sync-owncloud.sh
+++ helpers/sync-owncloud.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
 #Sync files using owncloud
+
+logger --tag "sync-owncloud.sh" "Syncing ownCloud..."
 $( pgrep oclient > /dev/null ) || /usr/local/bin/oclient > /dev/null
+logger --tag "sync-owncloud.sh" "Syncing done."
