7dfe0e7cac02d8ad8dc0b32cf04bd9c1955a15da
Lars Falk-Petersen <dev@falk-petersen.no>
2014-02-09 20:40:16 +0100

    Added midori

--- helpers/kill.sh
+++ helpers/kill.sh
@@ -5,4 +5,4 @@
 . /home/pi/photOS/helpers/include.sh
 
 logger --tag "kill.sh" "Stopping existing slideshows, videos, streams."
-killall feh vlc mplayer > /dev/null 2>&1
+killall midori feh vlc mplayer > /dev/null 2>&1
